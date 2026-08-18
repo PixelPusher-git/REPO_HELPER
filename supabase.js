2fc398f3337c8525a7f011a96693170afd1e16ba
Mr.Glitch <pixelpusher.studio@yandex.ru>
2026-08-18 21:38:25 +0300

    add safe deploy

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 // supabase.js
-// Вставь сюда свои значения из Project Settings → API
-const SUPABASE_URL = 'https://noixytxiokaagwvtfbfd.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_9yg6XrL_jyulbNQa3Xt0oA_a3fALhs5';
+const SUPABASE_URL = window.__env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = window.__env.SUPABASE_ANON_KEY;
 
-// Создаём клиент и делаем его глобальным
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-console.log('Supabase initialized:', !!window.supabase);
+console.log("Supabase initialized:", !!window.supabase);
